83d4605e4cff60f1de2aced5a765d29f3adec52e
Ilya Khorobrykh <horobryh17@gmail.com>
2022-07-23 13:51:46 +0300

    Added authAPI

---
+++ src/api/authAPI.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const instance = axios.create({
+    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    withCredentials: true,
+});
+export const authAPI = {
+    login: async (email: string, password: string, rememberMe: boolean) => {
+        return instance.post(`auth/login`, { email, password, rememberMe });
+    },
+};
