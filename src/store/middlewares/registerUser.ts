c7bd1d51c44a4392af164f9cf324b35a19244616
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-07-25 20:18:38 +0300

    feat: add setAuthErrorAC() to registerUser thunk

--- src/store/middlewares/registerUser.ts
+++ src/store/middlewares/registerUser.ts
@@ -2,20 +2,26 @@ import axios, { AxiosError } from 'axios';
 
 import { authAPI } from 'api';
 import { RegisterUserErrorType, UserDataType } from 'api/types';
+import { REQUEST_STATUS } from 'enums';
+import { setAuthErrorAC } from 'store/actions';
+import { setAppStatusAC } from 'store/actions/app';
 import { AppThunkType } from 'store/types';
 
 export const registerUser =
     ({ email, password }: UserDataType): AppThunkType =>
-    async () => {
+    async dispatch => {
         try {
+            dispatch(setAppStatusAC(REQUEST_STATUS.LOADING));
             await authAPI.register({ email, password });
         } catch (err) {
             const error = err as AxiosError | RegisterUserErrorType;
 
             if (axios.isAxiosError(error)) {
-                console.warn(error.message);
+                dispatch(setAuthErrorAC(error.message));
             } else {
-                console.warn(error.error);
+                dispatch(setAuthErrorAC(error.error));
             }
+        } finally {
+            dispatch(setAppStatusAC(REQUEST_STATUS.IDLE));
         }
     };
